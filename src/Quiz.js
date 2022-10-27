2c54c4827dad1a0a034e757496a73b8001f28bcb
JRose117 <joerose123@hotmail.co.uk>
2022-10-27 15:49:54 +0100

    loops and handles errors

--- src/Quiz.js
+++ src/Quiz.js
@@ -7,9 +7,17 @@ const Quiz = () => {
 const [questions, setQuestions] = useState(orderedQuestions)
 const [reveal, setReveal] = useState(false)
 const [count, setCount] = useState(0)
+const [disable, setDisable] = useState(false);
 
 const revealAnswer = () => {
     setReveal(reveal => !reveal)
+}
+
+const handleRefresh = () => {
+  setCount(0)
+  setQuestions(orderedQuestions)
+  firstQuestion()
+  setDisable(false)
 }
 
 const firstQuestion = () => {
@@ -31,6 +39,11 @@ console.log(questions)
 
 const handleButtonClick = (event) => {
   if(event.target.name === "correct") {
+    if (questions.length === 1){
+      console.log("1")
+      setDisable(true)
+    }
+    console.log(disable)
     questions.shift()
     newQuestion()
     setReveal(false)
@@ -46,18 +59,25 @@ const handleButtonClick = (event) => {
 
 return( 
   <>
-    <h1>{questions[0].Question}</h1>
-    <h2>{reveal && questions[0].Answer}</h2>
-      <div className="answerButtons">
-        <button name="correct" value="yes" id="yes" onClick={handleButtonClick}>✔️</button>
-      </div>
-      <div className="answerButtons">
-        <button name="wrong" value="no" id="no" onClick={handleButtonClick} >✕</button>
-      </div>
-      <div className="showAnswer">
-        <button name="reveal" value="reveal" id="reveal" onClick={revealAnswer} >reveal answer</button>
+    <div className="Top">
+      <h1> {!disable ? questions[0].Question : "Answered All Questions Please Refresh"} </h1>
+      <h2>{reveal && questions[0].Answer}</h2>
+      {disable && <button name="refresh" value="refresh" id="refresh" onClick={handleRefresh}> Refresh </button>}
+    </div>
+    <div className ="Bottom">
+      <div className="Buttons">
+        <div className="answerButtons">
+          <button name="wrong" disabled={disable} value="no" id="no" onClick={handleButtonClick} >✕</button>
+        </div>
+        <div className="showAnswer">
+          <button name="reveal" disabled={disable} value="reveal" id="reveal" onClick={revealAnswer} >reveal answer</button>
+        </div>
+        <div className="answerButtons">
+          <button name="correct" disabled={disable} value="yes" id="yes" onClick={handleButtonClick}>✔️</button>
+        </div>
       </div>
       <h3> You have got {count} out of {orderedQuestions.length} correct so far!</h3>
+      </div>
 
   </>
 )
